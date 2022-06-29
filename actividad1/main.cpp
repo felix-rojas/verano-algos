4435e1c50e0548589fe9c8cd2d51bcaacdb454ba
felix-rojas <A01201946@tec.mx>
2022-06-28 19:13:48 -0500

    added actividad1

---
+++ actividad1/main.cpp
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+// Calcule la sumatoria de 1 hasta n con un método iterativo
+unsigned int sumaIterativa(uint &n)
+{
+    unsigned int accum = 0;
+    while (n != 0)
+    {
+        accum += n;
+        n--;
+    }
+    return accum;
+}
+
+// Calcule la sumatoria de 1 hasta n con un método recursivo
+unsigned int sumaRecursiva(uint n)
+{
+    if (n = 1){
+        return 1;
+    }
+    return n + sumaRecursiva(n-1);
+}
+
+// Calcule el n-ésimo numero fibonacci con un método iterativo
+unsigned int fibonacciIterativo(uint n)
+{
+}
+
+// Calcule el n-ésimo numero fibonacci con un método recursivo
+unsigned int fibonacciRecursivo(uint n)
+{
+}
+
+int main
+{
+
+    return 0;
+}
