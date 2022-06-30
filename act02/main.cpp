b18356c2375778b741de0a25d5ea13370e307222
felix-rojas <A01201946@tec.mx>
2022-06-30 14:50:51 -0500

    all cases done

--- act02/main.cpp
+++ act02/main.cpp
@@ -1,21 +1,42 @@
 # include <stdio.h>
+# include <iostream>
 # include <vector>
 
 unsigned int factorialRecursivo(unsigned int n){
-    
+    if(n <= 1){
+        return 1;
+    }
+    return n * factorialRecursivo(n-1);
 }
 unsigned int factorialIterativo(unsigned int n){
-    
+    int count = n;
+    for (int i = 2; i < count; i++)
+    {
+        n*=i;
+    }
+    return n;
 }
-unsigned int potencialRecursivo(unsigned int m, unsigned int n){
-
+size_t potencialRecursivo(size_t n, unsigned int m){
+    if (m == 1){
+        return n;
+    }
+    return n*potencialRecursivo(n,m-1);
 }
-unsigned int potencialIterativo(unsigned int m, unsigned int n){
 
+size_t potencialIterativo(unsigned int n, unsigned int m){
+    size_t num = n;
+    for (int i = 1; i < m; i++){
+        num = num * n;
+    }
+    return num;
 }
 
 int main()
 {
-    /* code */
+    std::cout << potencialIterativo(12,15) << std::endl;
+    std::cout << potencialRecursivo(12,15) << std::endl;
+    std::cout << factorialIterativo(8) << std::endl;
+    std::cout << factorialRecursivo(8) << std::endl;
+
     return 0;
 }
