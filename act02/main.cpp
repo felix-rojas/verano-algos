0932f66eda7ae76653b050de1f24ef117df52948
felix-rojas <A01201946@tec.mx>
2022-06-30 14:07:34 -0500

    act02 first commit

---
+++ act02/main.cpp
@@ -0,0 +1,21 @@
+# include <stdio.h>
+# include <vector>
+
+unsigned int factorialRecursivo(unsigned int n){
+    
+}
+unsigned int factorialIterativo(unsigned int n){
+    
+}
+unsigned int potencialRecursivo(unsigned int m, unsigned int n){
+
+}
+unsigned int potencialIterativo(unsigned int m, unsigned int n){
+
+}
+
+int main()
+{
+    /* code */
+    return 0;
+}
