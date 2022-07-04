bf9b0ee723d339d6c57e84a57f5017403e913dce
felix-rojas <A01201946@tec.mx>
2022-07-04 09:54:12 -0500

    insertion sort listo

--- act06/main.cpp
+++ act06/main.cpp
@@ -62,12 +62,16 @@ void selectionSort(vector<int> &vector, int &compara, int &nSwaps)
 // bubbleSort
 // mueve el elemento mas grande hasta arriba
 
-void bubbleSort(vector<int> &vector, int &compara, int &nSwaps) {
+void bubbleSort(vector<int> &vector, int &compara, int &nSwaps)
+{
   compara = nSwaps = 0;
-  for(int i = 0; i < vector.size() - 1; i++) {
-    for(int j = 0; j < vector.size() - i - 1; j++) {
+  for (int i = 0; i < vector.size() - 1; i++)
+  {
+    for (int j = 0; j < vector.size() - i - 1; j++)
+    {
       compara++;
-      if(vector[j]  > vector[j + 1]) {
+      if (vector[j] > vector[j + 1])
+      {
         std::swap(vector[j], vector[j + 1]);
         nSwaps++;
       }
@@ -75,52 +79,71 @@ void bubbleSort(vector<int> &vector, int &compara, int &nSwaps) {
   }
 }
 
-while (swapped)
-int main(void)
-{
-  int i, tmp, size, flag;
-  // Inicio conteo de tiempo de ejecución
-  auto startTime = std::chrono::high_resolution_clock::now();
-  cin >> size;
-  cout << "Size: " << size << endl;
-  cin >> flag;
-  cout << "Print flag: " << flag << endl;
-  // Declaración del
-  vector<int> myVector;
-  // Lectura de los elementos del arreglo
-  for (i = 0; i < size; i++)
-  {
-    cin >> tmp;
-    myVector.push_back(tmp);
-  }
-  if (flag)
-  {
-    cout << "Los elementos del vector son: " << endl;
-    for (i = 0; i < size; i++)
-    {
-      cout << myVector[i] << " ";
+// Insertion sort
+// mejor caso es preordenado, con la comparación
+// del ciclo for ya no entra al ciclo while
+void insertionSort(vector<int> &vector, int &compara, int &nSwaps) {
+  int j = 0;
+  compara = nSwaps = 0;
+  for (int i = 1; i < vector.size(); i++) {
+    int key = vector[i];
+    j = i-1;
+    compara++;
+    while ( j >= 0 && vector[j] > key) {
+      vector[j+1] = vector[j];
+      j--;
+      nSwaps++;
+      compara++;
     }
-    cout << endl;
+    vector[j+1] = key;
   }
-  int comparaciones = 0;
-  int intercambios = 0;
-  swapSort(myVector, comparaciones, intercambios);
-  if (flag)
-  {
-    cout << "Los elementos del vector ordenado son: " << endl;
-    for (i = 0; i < size; i++)
-    {
-      cout << myVector[i] << " ";
-    }
-    cout << endl;
-  }
-
-  // Termina conteo de tiempo de ejecución
-  auto endTime = std::chrono::high_resolution_clock::now();
-  auto totalTime = endTime - startTime;
-
-  cout << "Tiempo de ejecución en ms: " << totalTime / std::chrono::milliseconds(1) << endl;
-  cout << "Numero de comparaciones: " << comparaciones << endl;
-  cout << "Numero de intercambios: " << intercambios << endl;
-  return 0;
 }
+
+  int main(void)
+  {
+    int i, tmp, size, flag;
+    // Inicio conteo de tiempo de ejecución
+    auto startTime = std::chrono::high_resolution_clock::now();
+    cin >> size;
+    cout << "Size: " << size << endl;
+    cin >> flag;
+    cout << "Print flag: " << flag << endl;
+    // Declaración del
+    vector<int> myVector;
+    // Lectura de los elementos del arreglo
+    for (i = 0; i < size; i++)
+    {
+      cin >> tmp;
+      myVector.push_back(tmp);
+    }
+    if (flag)
+    {
+      cout << "Los elementos del vector son: " << endl;
+      for (i = 0; i < size; i++)
+      {
+        cout << myVector[i] << " ";
+      }
+      cout << endl;
+    }
+    int comparaciones = 0;
+    int intercambios = 0;
+    swapSort(myVector, comparaciones, intercambios);
+    if (flag)
+    {
+      cout << "Los elementos del vector ordenado son: " << endl;
+      for (i = 0; i < size; i++)
+      {
+        cout << myVector[i] << " ";
+      }
+      cout << endl;
+    }
+
+    // Termina conteo de tiempo de ejecución
+    auto endTime = std::chrono::high_resolution_clock::now();
+    auto totalTime = endTime - startTime;
+
+    cout << "Tiempo de ejecución en ms: " << totalTime / std::chrono::milliseconds(1) << endl;
+    cout << "Numero de comparaciones: " << comparaciones << endl;
+    cout << "Numero de intercambios: " << intercambios << endl;
+    return 0;
+  }
