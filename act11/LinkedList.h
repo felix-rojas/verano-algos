cc9d490cbaba3e1921ce6909ca39a166cfe9d550
felix-rojas <A01201946@tec.mx>
2022-07-11 08:43:17 -0500

    clase pasada

---
+++ act11/LinkedList.h
@@ -0,0 +1,198 @@
+#ifndef _LINKEDLIST_H_
+#define _LINKEDLIST_H_
+
+#include "Node.h"
+#include <iostream>
+
+template <class T>
+class LinkedList {
+  private:
+    Node<T> *head;
+    Node<T> *tail;
+    int numElements;
+
+  public:
+    LinkedList();
+    ~LinkedList();
+    int getNumElements();
+    void printList();
+    void addFirst(T value);
+    void addLast(T value);
+    bool deleteData(T value);
+    bool deleteAt(int posicion);
+    T getData(int position);
+    void updateData(T value, T newValue);
+};
+
+template<class T>
+LinkedList<T>::LinkedList() {
+  std::cout << "--- Creando una lista vacia --- " << &head << std::endl;
+  head = nullptr;
+  tail = nullptr;
+  numElements = 0;
+}
+
+template<class T>
+LinkedList<T>::~LinkedList() {
+  std::cout << "--- Liberando memoria de la lista --- " << &head << std::endl;
+  Node<T> *prev, *q;
+  prev = head;
+  while (prev != nullptr) {
+    q = prev->next;
+    delete prev;
+    prev = q;
+  }
+  head = nullptr;
+  tail = nullptr;
+  numElements = 0;
+}
+
+template<class T>
+int LinkedList<T>::getNumElements() {
+  return numElements;
+}
+
+template<class T>
+void LinkedList<T>::printList() {
+  Node<T> *ptr = head;
+  while (ptr != nullptr) {
+    std::cout << ptr->data << " ";
+    ptr = ptr->next;
+  }
+  std::cout << std::endl;
+}
+
+template<class T>
+void LinkedList<T>::addFirst(T value) {
+  // creando un nuevo nodo de la lista
+  Node<T> *newNode = new Node<T>(value);
+  // Si la lista esta vacia if(head == nullptr)
+  if (numElements == 0) {
+    head = newNode;
+    tail = newNode; 
+  } 
+  else {
+    newNode->next = head;
+    head = newNode;
+  }
+  numElements++;
+}
+
+template<class T>
+void LinkedList<T>::addLast(T value) {
+  // Si la lista esta vacia if(head == nullptr)
+  if (numElements == 0) {
+    addFirst(value);
+  }
+  else {
+    // creando un nuevo nodo de la lista
+    Node<T> *newNode = new Node<T>(value);
+    tail->next = newNode;
+    tail = newNode;
+    numElements++;
+  }
+}
+
+template<class T>
+bool LinkedList<T>::deleteData(T value) {
+  bool estado = false;
+  // Si la lista esta vacia
+  if (numElements == 0) {
+    return estado;
+  }
+  else {
+    Node<T> *p = head;
+    Node<T> *prev = nullptr;
+    // buscar value en la lista
+    while (p != nullptr && p->data != value) {
+      prev = p;
+      p = p->next;
+    }
+    // si no existe value en la lista
+    if (p == nullptr)
+      return estado;
+    // Si debe borrarse el primer elemento de la lista
+    if (p == head) {
+      head = p->next;
+    } else if (p->next == nullptr) { // borrar ultimo en la lista
+      prev->next = nullptr;
+      tail = prev;     
+    } else {
+      prev->next = p->next;
+    }
+    delete p;
+    numElements--;
+    estado = true;
+    return estado;
+  }
+}
+
+template<class T>
+bool LinkedList<T>::deleteAt(int position) {
+  if (position < 0 || position >= numElements) {
+    throw std::out_of_range("Indice fuera de rango");
+  }
+  else if (position == 0) { // Si debe borrarse el primer elemento
+    Node<T> *p = head;
+    // Si la lista contiene solo un nodo
+    if (head != nullptr && head->next == nullptr) {
+      head = nullptr;
+      tail = nullptr;
+    }
+    else {
+      head = p->next;
+    }
+    delete p;
+    numElements--;
+    return true; 
+  }
+  else { // Si la lista contiene mas de un nodo
+    Node<T> *p = head, *q = nullptr;
+    int index = 0;
+    // Se busca el indice del elemento a borrar
+    while (index != position) {
+      q = p;
+      p = p->next;
+      index++;
+    }
+    // Si debe borrarse el último elemento
+    if (p->next == nullptr) {
+      q->next = nullptr;
+      tail = q;
+    }
+    else { // Cualquier otro elemento en medio de la lista 
+      q->next = p->next;
+    }
+    delete p;
+    numElements--;
+    return true;
+  }
+}
+
+template<class T>
+T LinkedList<T>::getData(int position) {
+  if (position < 0 || position >= numElements) {
+    throw std::out_of_range("Indice fuera de rango");
+  }
+  else {
+    if (position == 0)
+      return head->data;
+    Node<T> *p = head;
+    int index = 0;
+    while (p != nullptr) {
+      if (index == position)
+        return p->data;
+      index++;
+      p = p->next;
+    }
+    return {};
+  }
+}
+
+template<class T>
+void LinkedList<T>::updateData(T value, T newValue) {
+    
+}
+
+
+#endif // _LINKEDLIST_H_
