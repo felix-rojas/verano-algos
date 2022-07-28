0d324178f187a3f28c6870a86a7166a6f92e82a2
felix-rojas <A01201946@tec.mx>
2022-07-28 15:27:37 -0500

    openhash works, anottating now

---
+++ actividad5.1/attempt3/main.cpp
@@ -0,0 +1,21 @@
+# include "openHash.h"
+int main()
+{
+    // array that contains keys to be mapped
+    int a[] = {15, 11, 27, 8, 12};
+    int n = sizeof(a) / sizeof(a[0]);
+
+    // insert the keys into the hash table
+    Hash<int, int> h(7); // 7 is count of buckets in
+               // hash table
+    for (int i = 0; i < n; i++)
+        h.insertItem(a[i]);
+
+    // delete 12 from hash table
+    h.remove(12);
+
+    // display the Hash table
+    h.print();
+
+    return 0;
+}
---
+++ actividad5.1/attempt3/openHash.h
@@ -0,0 +1,109 @@
+#ifndef _OPENHASH_H_
+#define _OPENHASH_H_
+// Code is referenced from https://www.geeksforgeeks.org/c-program-hashing-chaining/
+// Article/Code was written by https://auth.geeksforgeeks.org/user/shubham_rana_77/articles
+// Code was edited by users rajeev0719singh shwetagaur4698 rishavpgl4 vikaspandey1206
+// CPP program to implement hashing with chaining
+
+#include <iostream>
+#include <list>
+#include <iterator>
+
+template <class T, class K>
+class Hash
+{
+    int BucketCount; // No. of buckets
+
+    // Pointer to an array containing buckets
+    std::list<T> *table;
+
+public:
+    Hash(T Cubetas); // Constructor
+
+    // inserts a keyVal into hash table
+    void insertItem(K keyVal);
+
+    // deletes a keyVal from hash table
+    void remove(K keyVal);
+    
+    // finds a keyVal from hash table, returns T
+    T find(K keyVal);
+
+    // hash function to map values to keyVal
+    int hashFunction(T value)
+    {
+        return (value % BucketCount);
+    }
+
+    void print();
+};
+
+template <class T, class K>
+Hash<T, K>::Hash(T cubetas)
+{
+    this->BucketCount = cubetas;
+    table = new std::list<T>[BucketCount];
+}
+
+template <class T, class K>
+void Hash<T, K>::insertItem(K keyVal)
+{
+    int index = hashFunction(keyVal);
+    table[index].push_back(keyVal);
+}
+
+template <class T, class K>
+void Hash<T, K>::remove(K keyVal)
+{
+    // get the hash index of keyVal
+    int index = hashFunction(keyVal);
+
+    // find the keyVal in (index)th list
+    std::list<int>::iterator i;
+    for (i = table[index].begin();
+         i != table[index].end(); i++)
+    {
+        if (*i == keyVal)
+            break;
+    }
+
+    // if keyVal is found in hash table, remove it
+    if (i != table[index].end())
+        table[index]
+            .erase(i);
+}
+
+template <class T, class K>
+T Hash<T, K>::find(K keyVal)
+{
+    // get the hash index of keyVal
+    int index = hashFunction(keyVal);
+
+    // find the keyVal in (index)th list
+    std::list<int>::iterator i;
+    for (i = table[index].begin();
+         i != table[index].end(); i++)
+    {
+        if (*i == keyVal)
+            break;
+    }
+
+    // if keyVal is found in hash table, return it
+    if (i != table[index].end())
+        return table[index];
+}
+
+// function to display hash table
+template <class T, class K>
+void Hash<T, K>::print()
+{
+    for (int i = 0; i < BucketCount; i++)
+    {
+        std::cout << i;
+        for (auto x : table[i])
+            std::cout << " --> " << x;
+        std::cout << std::endl;
+    }
+}
+
+#endif // _OPENHASH_H_
